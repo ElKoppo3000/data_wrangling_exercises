56c64d60cb167021e41d834606a272c0530f84a6
ElKoppo3000 <koplederjulian@gmail.com>
2023-08-30 17:49:19 +0200

    Completed exercise

--- hitting_the_road_with_citybike.py
+++ hitting_the_road_with_citybike.py
@@ -13,3 +13,42 @@
 #   b. If the user type is customer, add 1 to customer count
 #   c. Otherwise, add 1 to other count
 # 4. print out results
+
+# import the csv library
+import csv
+
+# open the file in read mode
+# file is in same folder than this script
+source_file = open('202009CitibikeTripdataExample.csv', 'r')
+
+# pass our source file as ingridient to the csv library's dict reader
+# store the result in citibike_reader
+citibike_reader = csv.DictReader(source_file)
+
+# print fieldnames (headers) of the dataset
+#print(citibike_reader.fieldnames)
+
+# count variables for subscriber, customer and other
+subscriber_count = 0
+customer_count = 0
+other_user_count = 0
+
+# iterate over dataset
+for row in citibike_reader:
+    # if usertype is subscriber
+    if row['usertype'] == "Subscriber":
+        # increment subscriber counter
+        subscriber_count += 1
+    # if usertype is customer
+    elif row['usertype'] == "Customer":
+        # increment subscriber counter
+        customer_count += 1
+    # catch the rest
+    else:
+        # increment other counter
+        other_user_count += 1
+
+# print results
+print('Subscribers: ' + str(subscriber_count))
+print('Customer: ' + str(customer_count))
+print('Other: ' + str(other_user_count))
